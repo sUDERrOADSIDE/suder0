1278cbb10ce5c96807e0d8200d58add607327251
sUDERrOADSIDE <35688749+sUDERrOADSIDE@users.noreply.github.com>
2018-02-02 14:30:17 +0530

    leapyear

---
+++ leapyear.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("ENTER THE VALUES OF a,b,c");
+scanf("%d",&a);
+if(a%4==0)
+ printf("LEAP YEAR");
+else
+printf("NOT A LEAP YEAR");
+return 0;
+}
+  
